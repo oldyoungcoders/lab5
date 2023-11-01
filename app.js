312cd7e167cceb7d79173bd640c54fb4ac160600
oldyoungcoders <132730577+oldyoungcoders@users.noreply.github.com>
2023-11-01 19:42:48 +0800

    Add files via upload
    lab5

---
+++ app.js
@@ -0,0 +1,295 @@
+"use strict"; 
+
+// var age = 5;
+// if(age >= 18){
+//     console.log("You are an adult");
+// } else if( 2 > age < 10){
+//     console.log("You are a child");
+// } else if (age <= 2){
+//     console.log("You are a baby");
+// } else {
+//     console.log("You are a teenager");
+// }
+
+
+
+
+// var c = "";
+// console.log(c);
+
+
+
+
+
+// var nas = 10;
+// var tasalbar ;
+// if(nas >= 18){
+//     tasalbar = 15000;
+//     console.log("Tanii nas " + nas + " uchir  " + tasalbar);
+// } else if( nas < 18){
+//     tasalbar = 8000;
+//     console.log("Tanii nas " + nas + " uchir  " + tasalbar);
+// } else if (nas <= 6){
+//     tasalbar = "unegui";
+//     console.log("Tanii nas " + nas + " uchir  " + tasalbar);
+// } else {
+//     console.log("Orj bolohgui ");
+// }
+
+
+
+
+
+// console.log(c);
+
+
+
+
+
+
+// var tsag = 15;
+// if (tsag <=6) {
+//     console.log("Uuriin mend");
+// } else if (tsag <= 12) {
+//     console.log("Ugluunii mend");
+// } else if (tsag <= 18) {
+//     console.log("Udriin mend");
+// } else  if (tsag <= 22){
+//     console.log("Oroin mend");
+// } else {
+//     console.log("Shuniin mend");
+// }
+
+
+
+
+
+
+// console.log(c);
+
+
+
+
+
+// var noOne = 15;
+// var noTwo = 15;
+// var nO = 2;
+
+// if (noOne > noTwo) {
+//     console.log("Ehnii too buyu " +  noOne + " ih baina");
+// } else if (noOne < noTwo) {
+//     console.log("Hoir dahi too buyu " + noTwo + " ih baina");
+// } else {
+//     console.log("Ene " + nO + " too tencuu baina . Ene bol ehnii too = " + noOne + " bol hoir dahi too = " + noTwo + " mun. ");
+// }
+
+
+
+
+
+
+
+// console.log(c);
+
+
+
+
+
+
+
+
+// var tooOne = 10;
+// var tooTwo = 10;
+// var tooThree = 11;
+
+// if (tooOne > tooTwo  && tooOne > tooThree) {
+//     console.log("Ehnii too buyu " +  tooOne + " ih baina");
+// } else if (tooTwo > tooOne && tooTwo > tooThree) {
+//     console.log("Hoir dahi too buyu " + tooTwo + " ih baina");
+// } else if (tooThree > tooOne && tooThree > tooTwo) {
+//     console.log("Guravdah dahi too buyu " + tooThree + " ih baina");
+// } else {
+//     console.log("Bugd tencuu baina.");
+// }
+
+
+
+
+
+
+// console.log(c);
+
+
+
+
+
+
+
+
+// var too = +prompt("Too oruulna uu");
+// if (too % 3 == 0 && too % 5 == 0) {
+//     console.log("Garaas oruulsan too 3 bolon 5 -nd huvaagdaj baina");
+// } else {
+//     console.log("Garaas oruulsan too 3 bolon 5 -nd huvaagdahgui baina");
+// }
+
+
+
+
+
+
+// console.log(c);
+
+
+
+
+
+
+
+
+// var day = +prompt("1-7 hurtel too oruulna uu");
+// switch (day) {
+//     case 1:
+//         console.log("Davaa");
+//         break;
+//     case 2:
+//         console.log("Maigmar");
+//         break;
+//     case 3:
+//         console.log("Lhagva");
+//         break;
+//     case 4:
+//         console.log("Purev");
+//         break;
+//     case 5:
+//         console.log("Baasan");
+//         break;
+//     case 6:
+//         console.log("Baimba");
+//         break;
+//     case 7:
+//         console.log("Naim");
+//         break;
+//     default:
+//         console.log("Not found");
+//         break;
+// }
+
+// var dun = +prompt("Chi shalgalt deeree hed avdag ve?");
+
+
+
+
+
+// console.log(c);
+
+
+
+
+
+// switch (true) {
+//     case dun<59:
+//         console.log("Davtdalgand aiv.'F' gedeg chin mash muu baina");
+//         break;
+//     case dun<69:
+//         console.log("F-iig bodvol D dajgui ch bagsh chin chamd ih uurlana daa !");
+//         break;
+//     case dun<79:
+//         console.log("Hmm ? C aig dundaj ch gesen hicheelee udur bolgon gertee davtaj baih shaardlagatai!!!");
+//         break;
+//     case dun<89:
+//         console.log("Sain baina. Ulam sain hicheegeerei! B dajgiu shuu!");
+//         break;
+//     case dun<=100:
+//         console.log("Mundag! Bayr hurgei . Uunsh gegee onc dun avch baigaarai! A ih sain.");
+//         break;
+//     default:
+//         console.log("1-100 hurtel l toog oruulah oistoi!");
+//         break;
+// }
+
+
+
+
+
+
+
+
+// console.log(c);
+
+
+
+
+
+
+// var too1 = +prompt("Ene bol toonii mashin. Ehnii toog oruulna uu");
+// var temdegt = prompt("Odoo harin temdegtee oruulna uu?");
+// var too2 = +prompt("Daraagiin toog oruulna uu");
+
+// switch (temdegt) {
+//     case "+":
+//         console.log(too1 + too2);
+//         break;
+//     case "-":
+//         console.log(too1 - too2);
+//         break;
+//     case "*":
+//         console.log(too1 * too2);
+//         break;
+//     case "/":
+//         console.log(too1 / too2);
+//         break;
+//     default:
+//         console.log("Zuvhun nemeh, hasah, urjih, huvaah temdegtee oruulna uu?");
+//         break;
+// }
+
+
+
+
+
+
+
+var fruit = "apple";
+
+switch (fruit) {
+    case "apple":
+    case "guzeelzgene":
+        console.log("Ulaan ungute jims");
+        break;
+    case "banana":
+    case "lemon":
+    case "mango":
+        console.log("Shar ungute jims");
+        break;
+} 
+
+
+
+
+// var a= 85, b= 75, c= 96, d= 69;
+// if(a>=80){
+//     a=a;
+// } else {
+//     a=0;
+// }
+
+// if(b>=80){
+//     b=b;
+// } else {
+//     b=0;
+// }
+
+// if(c>=80){
+//     c=c;
+// } else {
+//     c=0;
+// }
+
+// if(d>=80){
+//     d=d;
+// } else {
+//     d=0;
+// }
+
+// console.log(a+b+c+d);
